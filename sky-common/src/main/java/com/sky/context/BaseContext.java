e163a6852167f4e48f303e2d67441ad28d25ba66
CangQTaleaway <517931160@qq.com>
2023-07-25 16:12:40 +0800

    苍穹外卖项目初始代码

---
+++ sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -0,0 +1,19 @@
+package com.sky.context;
+//上下文相关的类
+public class BaseContext {
+
+    public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
+
+    public static void setCurrentId(Long id) {
+        threadLocal.set(id);
+    }
+
+    public static Long getCurrentId() {
+        return threadLocal.get();
+    }
+
+    public static void removeCurrentId() {
+        threadLocal.remove();
+    }
+
+}
