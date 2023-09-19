fc81bcc264a9a3ccc43580df27fbb3d6e8f359b9
CangQTaleaway <517931160@qq.com>
2023-09-19 12:05:50 +0800

    缓存套餐功能业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -10,6 +10,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -35,6 +36,7 @@ public class SetmealController {
     * */
     @PostMapping
     @ApiOperation("新增套餐")
+    @CacheEvict(cacheNames = "setmealCache",key="#setmealDTO")//清理缓存数据
     public Result sava(@RequestBody SetmealDTO setmealDTO){
         log.info("新增套餐：{}",setmealDTO);
         setmealService.saveWithDish(setmealDTO);
@@ -57,6 +59,7 @@ public class SetmealController {
     * */
     @DeleteMapping
     @ApiOperation("批量删除套餐")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)//清理所有缓存数据
     public Result delete(@RequestParam List<Long> ids){
         log.info("根据id批量删除套餐：{}",ids);
         setmealService.deleteBatch(ids);
@@ -79,6 +82,7 @@ public class SetmealController {
     * */
     @PutMapping
     @ApiOperation("修改套餐")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)//清理所有缓存数据
     public Result update(@RequestBody SetmealDTO setmealDTO){
         log.info("修改套餐：{}",setmealDTO);
         setmealService.update(setmealDTO);
@@ -90,6 +94,7 @@ public class SetmealController {
     * */
     @PostMapping("/status/{status}")
     @ApiOperation("套餐起售或停售")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)//清理所有缓存数据
     public Result startOrStop(@PathVariable Integer status,Long id){
         setmealService.startOrStop(status,id);
         return Result.success();
